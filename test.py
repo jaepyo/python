acaaef8a0170753c245788eff9f9f2112d133a53
A70894 <jps@cnspartner.com>
2022-10-17 21:20:09 +0900

    22.10.17

--- test.py
+++ test.py
@@ -1,41 +1,46 @@
-# def func_a(arr): # 어떤 숫자가 몇 개있는지 Count하는 함수
-#     counter = [0 for _ in range(1001)] # Count in List, 0 값이 index 1000까지 삽입
-#     for x in arr:
-#         counter[x] += 1 
-#     return counter
+# def solution(s):
+#     answer = s
+#     num_s = { 'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9};
+    
+#     for item in num_s.items():
+#         answer = answer.replace(item[0], str(item[1]))
+#     return int(answer)
 
-# #The following is code to output testcase.
-# arr = [1, 2, 3, 3, 1, 3, 3, 2, 3, 2]
+# #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+# s1 = "one4seveneight"
+# ret1 = solution(s1)
 
-# print(func_a(arr))
+# #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+# print("solution 함수의 반환 값은", ret1, "입니다.")
 
-# words = ["CODE", "COED", "CDEO"]
-# word = "CODE"
+# s2 = "23four5six7"
+# ret2 = solution(s2)
 
-# count = 0
+# #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+# print("solution 함수의 반환 값은", ret2, "입니다.")
 
-# for comp in words:
-#     for x, y in zip(comp, word):
-#         print(x, y)
+# s3 = "2three45sixseven"
+# ret3 = solution(s3)
 
-# used_tv = [0] * 25
-# print(used_tv)
+# #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+# print("solution 함수의 반환 값은", ret3, "입니다.")
 
-# for entry in enumerate(['A','B','C']):
-#     print(entry)
+# s4 = "123"
+# ret4 = solution(s4)
 
-#
-# n = 4
-# bundle = "cacdbdedccbb"
+# #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+# print("solution 함수의 반환 값은", ret4, "입니다.")
 
+# answer = "2three45sixseven"
+# num_s = { 'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9};
+# # for item in num_s.items():
+# #         answer = answer.replace(item[0], str(item[1]))
+# # print(answer)
 
-# def func_a(bundle, start):
-#     return bundle[start::2]
+# for item in num_s.items():
+#     print(item)
 
-# a_cards = func_a(bundle, 0)[:n]
+# answer = "2three45sixseven"
+# for i in answer:
+#     print(i)
 
-# print(a_cards)
-
-time_table = [1, 1, 0, 0, 1, 0, 1, 0, 0, 0]
-for i, t in reversed(list(enumerate(time_table))):
-    print(i, t)
